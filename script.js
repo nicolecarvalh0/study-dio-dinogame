1434bebcd0c877da01e4a0660e2154225e728557
Nicole <nicolecarvalho.ti@hotmail.com>
2021-02-07 02:28:32 -0300

    Fixing cactus

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
-        if(!isJumping) {
+        if (!isJumping) {
             jump();
         }
     }
 }
 
-function jump() {
-    let position = 0;
+function jump() {   
 
     isJumping = true;
 
     let upInterval = setInterval(() => {
         if (position >= 150) {
-            
+
             clearInterval(upInterval);
 
             //descendo
             let downInterval = setInterval(() => {
-                if(position <= 0) {
+                if (position <= 0) {
                     clearInterval(downInterval);
                     isJumping = false;
                 } else {
@@ -36,7 +36,7 @@ function jump() {
             //subindo
             position += 20;
             dino.style.bottom = position + 'px';
-            
+
         }
     }, 20);
 
@@ -45,10 +45,29 @@ function jump() {
 function createCactus() {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
 
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if(cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            //Game Over
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo!</h1>';
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
 }
 
 createCactus();
